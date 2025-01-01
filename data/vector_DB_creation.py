a6349500a9b15f1e9a1819b28fc26da1e66cc8f4
irazig <ilias.razig@ensae.fr>
2025-01-01 19:08:29 +0000

    class for vector database creation/loading

--- data/vector_DB_creation.py
+++ data/vector_DB_creation.py
@@ -1,26 +1,27 @@
+import os
 from dataloaders import document_loader
 from dataloaders import pdf_loader
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores import FAISS
 
 
-current_directory = os.getcwd()
-json_file_path = os.path.join(current_directory, "api_response.json")
+class vector_DB :
+    def __init__(self, database_name, save_dir):
+        self.database_name = database_name
+        self.save_dir = save_dir
+        self.database_file_path = os.path.join(save_dir, database_name)
 
-with open(json_file_path, 'r') as j:
-    data = json.loads(j.read())
+    def create_vector_DB(self, document, embedding_model):
+        document_path = os.path.join(self.save_dir, document)
+        # We split the the json into documents
+        docs_after_split = document_loader(document_path, chunk_size = 2000, chunk_overlap  = 200)
 
-# We split the the json into documents
-docs_after_split = document_loader(json_file_path, chunk_size = 700, chunk_overlap  = 50)
+        # Creation of the vectorDB with the embeddings
+        vectorstore = FAISS.from_documents(docs_after_split, embedding_model)
+        vectorstore.save_local(self.database_file_path)
+        return vectorstore
 
+    def load_vector_DB(self, embedding_model):
+        vectorstore = FAISS.load_local(self.database_file_path, embedding_model, allow_dangerous_deserialization=True)
+        return vectorstore
 
-# We load the langchain embedding model that is on hugging face
-huggingface_embeddings = HuggingFaceBgeEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5",
-    model_kwargs={'device':'cpu'}, 
-    encode_kwargs={'normalize_embeddings': True}
-)
-
-# Creation of the vectorDB with the embeddings
-vectorstore = FAISS.from_documents(docs_after_split, huggingface_embeddings)
-vectorstore.save_local("faiss_index")
